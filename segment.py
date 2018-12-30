c8b80c67c35cbc40889243888707b65dccc0faab
kjetv <kjetv@stud.ntnu.no>
2018-12-31 00:04:54 +0100

    Added center hand and circle

--- segment.py
+++ segment.py
@@ -32,12 +32,38 @@ def segment(image, threshold=12):
                                 255,
                                 cv2.THRESH_BINARY)[1]
 
+    
+
+    #-------------- My own touch-------------------------------
+   
+    ## Copy the thresholded image.
+    im_floodfill = thresholded.copy()
+	 
+	# Mask used to flood filling.
+	# Notice the size needs to be 2 pixels than the image.
+    h, w = thresholded.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+	 
+	# Floodfill from point (0, 0)
+    cv2.floodFill(im_floodfill, mask, (0,0), 255);
+	 
+	# Invert floodfilled image
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+	 
+	# Combine the two images to get the foreground.
+    thresholded = thresholded | im_floodfill_inv
+   
+ 	#---------------------------------------------------------
+
+
+
     # get the contours in the thresholded image
-    (_, cnts, _) = cv2.findContours(thresholded.copy(),
+    _,cnts,_ = cv2.findContours(thresholded.copy(),
                                     cv2.RETR_LIST,
                                     cv2.CHAIN_APPROX_SIMPLE)
+    
 
-    # return None, if no contours detected
+
     if len(cnts) == 0:
         return
     else:
@@ -45,7 +71,15 @@ def segment(image, threshold=12):
         segmented = max(cnts, key=cv2.contourArea)
         epsilon = 0.01*cv2.arcLength(segmented,True)
         approx = cv2.approxPolyDP(segmented,epsilon,True)
-        return (thresholded, approx)
+
+
+        cnt = approx
+        (x,y),radius = cv2.minEnclosingCircle(cnt)
+        center = (int(x),int(y))
+        radius = int(radius)
+        out = cv2.linearPolar(thresholded, center, 100, cv2.INTER_LINEAR+cv2.WARP_FILL_OUTLIERS)
+        print(radius)
+        return (out, approx, center, radius)
 
 #-------------------------------------------------------------------------------
 # Main function
@@ -86,7 +120,7 @@ if __name__ == "__main__":
 
         # convert the roi to grayscale and blur it
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
         # to get the background, keep looking till a threshold is reached
         # so that our running average model gets calibrated
@@ -100,10 +134,12 @@ if __name__ == "__main__":
             if hand is not None:
                 # if yes, unpack the thresholded image and
                 # segmented region
-                (thresholded, segmented) = hand
+                (thresholded, segmented, center, radius) = hand
 
                 # draw the segmented region and display the frame
+                cen = (center[0]+right,center[1]+top)
                 cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                cv2.circle(clone, cen, radius, (255,0,0), 2)
                 cv2.imshow("Thesholded", thresholded)
 
         # draw the segmented hand
